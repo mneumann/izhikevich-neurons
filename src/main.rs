45b978e6ad416686c05033b25b9a4f472f37eb4c
Michael Neumann <mneumann@ntecs.de>
2015-08-18 21:46:00 +0200

    Multiple plots

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,47 @@
 extern crate izhikevich_neurons;
 extern crate gnuplot;
 
-use izhikevich_neurons::{Config, State};
+use izhikevich_neurons::{Config, State, Num};
 use gnuplot::{Figure, Caption, Color, AxesCommon};
 
 fn main() {
     let config = Config::regular_spiking();
-    let mut neuron = State::new();
-    let mut time = 0.0;
 
+    const PARAMS : &'static [(&'static str, Num, &'static str)] = &[
+        ("Neuron 1 [-7 uA current 200..700ms]", -7.0, "blue"),
+        ("Neuron 2 [-2.69 uA current 200..700ms]", -2.69, "red"),
+        ("Neuron 3 [-2.7 uA current 200..700ms]", -2.7, "green")
+    ];
+
+    let mut neurons: Vec<_> = PARAMS.iter().map(|_| State::new()).collect();
+    let mut potentials: Vec<_> = PARAMS.iter().map(|_| Vec::new()).collect();
+
+    let mut time = 0.0;
     let mut times = Vec::new();
-    let mut potentials = Vec::new();
 
     while time < 1_000.0 {
         // record current state
         times.push(time);
-        potentials.push(neuron.potential());
+        for (i, &mut neuron) in neurons.iter_mut().enumerate() {
+            potentials[i].push(neuron.potential());
+        }
         time += 1.0;
 
         // update state
-        let syn_i = if time >= 200.0 && time <= 700.0 { -7.0 } else { 0.0 };
-        neuron = neuron.step_1ms(syn_i, &config);
+        for (i, mut neuron) in neurons.iter_mut().enumerate() {
+            let syn_i = if time >= 200.0 && time <= 700.0 { PARAMS[i].1 } else { 0.0 };
+            *neuron = neuron.step_1ms(syn_i, &config);
+        }
     }
 
     let mut fg = Figure::new();
-    fg.axes2d().
-        set_x_label("time [ms]", &[]).
-        set_y_label("potential [mV]", &[]).
-        lines(times.iter(), potentials.iter(), &[Caption("Neuron potential over time"), Color("black")]);
+    {
+        let mut diag = fg.axes2d().
+            set_x_label("time [ms]", &[]).
+            set_y_label("potential [mV]", &[]);
+        for (i, &p) in PARAMS.iter().enumerate() {
+            diag.lines(times.iter(), potentials[i].iter(), &[Caption(p.0), Color(p.2)]);
+        }
+    }
     fg.show();
 }
